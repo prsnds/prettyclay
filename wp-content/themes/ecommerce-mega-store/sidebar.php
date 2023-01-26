7d30af53aee2867f406652f0371ea2d8339301e1
prsnds <pjeetstrtk@gmail.com>
2023-01-26 16:47:11 +0600

    26012023

--- wp-content/themes/ecommerce-mega-store/sidebar.php
+++
@@ -1,5 +0,0 @@
-<div class="sidebar-area mt-5">
-  <?php
-    dynamic_sidebar('ecommerce-mega-store-sidebar');
-  ?>
-</div>
--- wp-content/themes/ecommerce-mega-store/template-parts/pagination.php
+++
@@ -1,5 +0,0 @@
-<?php
-	the_posts_pagination( array(
-		'prev_text' => esc_html__( 'Previous page', 'ecommerce-mega-store' ),
-		'next_text' => esc_html__( 'Next page', 'ecommerce-mega-store' ),
-	) );
